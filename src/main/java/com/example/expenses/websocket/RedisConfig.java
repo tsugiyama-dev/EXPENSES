ac34379ccb25ddda8107291a1cfc8933e7ab6c18
Tsugiyama <TOKAI1107@gmail.com>
2026-04-28 16:39:40 +0900

    redis 実装

---
+++ src/main/java/com/example/expenses/websocket/RedisConfig.java
@@ -0,0 +1,16 @@
+package com.example.expenses.websocket;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+
+	
+//	@Bean
+//	RedisTemplate<String, RedisNotificationMessage> redisTemplate(
+//			RedisConnectionFactory connectionFactory) {
+//		
+//		var template = new RedisTemplate<String, RedisNotificationMessage>();
+		
+//	}
+}
